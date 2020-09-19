05cd31c1a8d4ed31515377daf0ac008bfddf795b
MR. SPYDER//007 <68064582+mrspyder007@users.noreply.github.com>
2020-09-19 03:59:05 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+
+function setup() {
+  createCanvas(450,450);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(0); 
+
+  fill(255);
+  textSize(24);
+  textStyle("bold")
+  text("tick tock clock",100,400);
+
+  translate(200,200);
+  rotate(-90);
+
+  hr=hour();
+  mm=minute();
+  sc=second();
+
+  strokeWeight(8);
+  stroke("red");
+  noFill();
+  var end = map(sc,0,60,0,360);
+  arc(0,0,300,300,0,end);
+  
+  stroke("green");
+  var end2 = map(mm,0,60,0,360);
+  arc(0,0,280,280,0,end2);
+  
+  stroke("blue");
+  var end3 = map(hr%12,0,12,-0,360);
+  arc(0,0,260,260,0,end3);
+  
+  push();
+  rotate(end);
+  stroke("red");
+  line(0,0,100,0);
+  pop();
+  
+  
+  push();
+  rotate(end2);
+  stroke("green");
+  line(0,0,75,0);
+  pop();
+  
+  push();
+  rotate(end3);
+  stroke("blue");
+  line(0,0,50,0);
+  pop();
+  
+  stroke("Black");
+  point (0,0);
+
+  }
+
+
+
+
+
+
+
